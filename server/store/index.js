f962df8eb7ffbc7d49724f4917eb28d45bc9628c
Ruslan Sharipov <slonru37@gmail.com>
2023-01-18 16:26:54 +0300

    add services

--- server/store/index.js
+++ server/store/index.js
@@ -1,32 +1,54 @@
 const countries = [
-    {
-      name: "thailand",
-      value: "TH",
-    },
-    {
-      name: "russia",
-      value: "RU",
-    },
-    {
-        name: "uited Kingdom",
-        value: "UK"
+  {
+    name: "thailand",
+    value: "TH",
+  },
+  {
+    name: "russia",
+    value: "RU",
+  },
+  {
+    name: "uited Kingdom",
+    value: "UK",
+  },
+];
+const services = [
+  {
+    name: "Discord",
+    value: "ds",
+  },
+  {
+    name: "Telegram",
+    value: "tg",
+  },
+  {
+    name: "Twitter",
+    value: "tw",
+  },
+  {
+    name: "Viu",
+    value: "wv",
+  },
+  {
+    name: "BigC",
+    value: "zu",
+  },
+  {
+    name: "LINE MAN",
+    value: "lim",
+  },
+  {
+    name: "The1",
+    value: "the",
+  },
+  {
+    name: "Trueid",
+    value: "tru",
+  },
+  {
+    name: "Line msg",
+    value: "me",
+  },
+];
 
-    },
-  ];
-  const services = [
-    {
-      name: "Discord",
-      value: "ds",
-    },
-    { 
-        name: "Telegram", 
-        value: "tg" 
-    },
-    { 
-        name: "Twitter", 
-        value: "tw" 
-    },
-  ];
-
-
-export default { countries, services}
+export default { countries, services };
